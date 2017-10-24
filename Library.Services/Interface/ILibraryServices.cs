75c402c111d52bda89e21b439f7d28a5df9303ca
Vladimir Strukelj <vladimir.strukelj@gmail.com>
2017-10-24 22:56:55 +0200

    more services

---
+++ Library.Services/Interface/ILibraryServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Data.Models;
+
+namespace Library.Services.Interface
+{
+    public interface ILibraryServices
+    {
+        //book services
+        IList<BOOK> GetBooksByRange(int index, int count);
+
+        IList<BOOK> GetBooksByAuthor(string author);
+
+        IList<BOOK> GetBooksByTitle(string title);
+
+        IList<BOOK> GetBooksByISBN(string isbn);
+
+        BOOK GetBookByISBN(string isbn);
+
+        bool AddBook(BOOK book);
+
+        bool DeleteBook(BOOK book);
+
+        bool EditBook(BOOK book);
+
+        //author services
+        IList<AUTHOR> GetAuthorsByName(string input);
+
+        bool DeleteAuthor(AUTHOR author);
+
+        bool AddAuthor(AUTHOR author);
+
+        bool EditAuthor(AUTHOR author);
+        
+        
+
+    }
+}
---
+++ Library.Services/LibraryServices.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Data.Models;
+using Library.Services.Interface;
+
+namespace Library.Services
+{
+    class LibraryServices :ILibraryServices
+    {
+        public IList<BOOK> GetBooksByRange(int index, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BOOK> GetBooksByAuthor(string author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BOOK> GetBooksByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BOOK> GetBooksByISBN(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BOOK GetBookByISBN(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddBook(BOOK book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteBook(BOOK book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditBook(BOOK book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<AUTHOR> GetAuthorsByName(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAuthor(AUTHOR author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddAuthor(AUTHOR author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditAuthor(AUTHOR author)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
